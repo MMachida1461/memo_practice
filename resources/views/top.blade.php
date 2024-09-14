e62ce63c2d512b8df9d352421e1c557e9da0b4b8
MMachida1461 <machida951461@gmail.com>
2024-09-14 17:43:16 +0900

    独自cssの階層を変更

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,11 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/top.css">
+    <link rel="stylesheet" href="/css/top.css">
     <title>メモ</title>
 </head>
 <body>
     {{-- ダミーのメモ一覧 start --}}
-    <h1>メモ一覧</h1>
+    <h1 class="h1">メモ一覧</h1>
+    <ul>
+        <li><a href="#">メモ1</a></li>
+        <li><a href="#">メモ2</a></li>
+        <li><a href="#">メモ3</a></li>
+        <li><a href="#">メモ4</a></li>
+        <li><a href="#">メモ5</a></li>
+        <li><a href="#">メモ6</a></li>
+        <li><a href="#">メモ7</a></li>
+        <li><a href="#">メモ8</a></li>
+        <li><a href="#">メモ9</a></li>
+        <li><a href="#">メモ10</a></li>
+    </ul>
+    {{-- ダミーのメモ一覧 end --}}
 </body>
 </html>
