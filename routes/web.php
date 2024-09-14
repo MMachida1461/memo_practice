a41a53120eec25a7c4b8e90ac43efcca1e279641
MMachida1461 <machida951461@gmail.com>
2024-09-14 18:09:46 +0900

    メモ作成ページの作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Monolog\Handler\RotatingFileHandler;
 
 Route::get('/memo', function () {
     return view('memo');
+});
+
+//まずは、idで呼び出した時のダミーのページを呼び出す処理を作成
+Route::get('/memo/create', function(){
+    return view('create');
 });
