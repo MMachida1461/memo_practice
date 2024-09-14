e391914389a418b9e9b0afcf6a983648e21f004c
MMachida1461 <machida951461@gmail.com>
2024-09-14 18:14:37 +0900

    メモの詳細を表示するページを作成

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ Route::get('/memo', function () {
     return view('memo');
 });
 
+// メモをDBから取得するページの作成
+Route::get('/memo/detail', function(){
+    return view('detail');
+});
+
 //まずは、idで呼び出した時のダミーのページを呼び出す処理を作成
 Route::get('/memo/create', function(){
     return view('create');
