0cb2746ae3d4beb361b57c8c72b018c16cdc4f60
MMachida1461 <machida951461@gmail.com>
2024-09-15 18:29:57 +0900

    DB→Model→Viewの処理

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Models\Memo;
 
 class MemoController extends Controller
 {
+    //メモ一覧画面へ遷移させる
+    public function index() {
+        $memos = Memo::all();
+        var_dump($memos);
+        return view('memo');
+    }
+
     ///memo/createのパスで渡ってきたら、detail.blade.phpを作成
     public function getMemo() {
         Log::debug("web.appからメソッドが呼び出されていることを確認");
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Monolog\Handler\RotatingFileHandler;
 use App\Http\Controllers\MemoController;
 
-Route::get('/memo', function () {
-    return view('memo');
-});
+//メモ一覧ページに遷移する際のルーティング。MemoContorollerに渡す
+Route::get('/memo', [MemoController::class, 'index']);
 
 // メモをDBから取得するページの作成
 Route::get('/memo/detail', [MemoController::class, 'getMemo']);
